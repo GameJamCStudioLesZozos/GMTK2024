7eb8166242a1c85df7bdf88d0dfe699e0c88da8f
ALEXALINE Thomas <thomas.alexaline@gmail.com>
2024-08-16 23:56:46 +0200

    updated gitignore + cleaned sol prefab

--- scripts/CollisionShape2dAutoFill.cs
+++ scripts/CollisionShape2dAutoFill.cs
@@ -6,14 +6,8 @@ public partial class CollisionShape2dAutoFill : CollisionPolygon2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		/*
-		Node node = GetParent();
-		while (node != null && node is not Polygon2D)
-			node = GetParent();
-		if (node is null)
-			return;
-		Polygon = (node as Polygon2D).Polygon;
-		*/
-		Polygon = GetParent().GetParent<Polygon2D>().Polygon;
+        Polygon2D polygon = GetNode<Polygon2D>("%GroundPolygon");
+		if (polygon != null)
+			Polygon = polygon.Polygon;
 	}
 }
