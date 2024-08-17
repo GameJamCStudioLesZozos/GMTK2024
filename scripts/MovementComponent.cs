55a44566d49f6682b18b2a544f69bb9c723e3c84
PoneyUHC <uhc.contact@laposte.net>
2024-08-17 17:17:28 +0200

    add force gravity

--- scripts/MovementComponent.cs
+++ scripts/MovementComponent.cs
@@ -9,12 +9,23 @@ public partial class MovementComponent : Node
 
 	[Signal]
 	public delegate void JumpedEventHandler(Vector2 impulse);
+	
+	[Export]
+	public float GravityMultiplier { get; set; } = 10f;
+
+	[Signal]
+	public delegate void ForceGravityEventHandler(float multiplier);
+
+	private bool forcing_gravity = false;
+
 
 	private List<Node> groundCollisions = new();
 
-	public override void _Process(double delta)
+
+    public override void _Process(double delta)
 	{
 		checkJump();
+		checkForceGravity();
 	}
 
 	public void _OnBodyEntered(Node node)
@@ -34,5 +45,14 @@ public partial class MovementComponent : Node
 		{
 			EmitSignal(SignalName.Jumped, new Vector2(0f, -JumpImpulse));
 		}
+	}
+
+	private void checkForceGravity()
+	{
+		float gravity_value = 1f;
+		if (Input.IsActionPressed("force_gravity"))
+			gravity_value = GravityMultiplier;
+
+		EmitSignal(SignalName.ForceGravity, gravity_value);
 	}
 }
