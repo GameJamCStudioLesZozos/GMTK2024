c781b9b14a46978d5df89bf989670f81d3e71691
ALEXALINE Thomas <thomas.alexaline@gmail.com>
2024-08-19 12:06:35 +0200

    update collisionShape2AutoFill with try get to avoid error

--- scripts/CollisionShape2dAutoFill.cs
+++ scripts/CollisionShape2dAutoFill.cs
@@ -6,7 +6,7 @@ public partial class CollisionShape2dAutoFill : CollisionPolygon2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-        Polygon2D polygon = GetNode<Polygon2D>("%GroundPolygon");
+        Polygon2D polygon = GetNodeOrNull<Polygon2D>("%GroundPolygon");
         if (polygon is null)
 			polygon = GetParent().GetParent<Polygon2D>();
 
